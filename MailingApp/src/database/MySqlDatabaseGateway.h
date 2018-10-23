f7454f7156929e6b310a8e9324be436ee39b2709
Daraenor <viriato22@gmail.com>
2018-10-23 09:30:28 +0200

    Begin

--- MailingApp/src/database/MySqlDatabaseGateway.h
+++ MailingApp/src/database/MySqlDatabaseGateway.h
@@ -25,11 +25,11 @@ public:
 private:
 
 	// Text buffers for ImGUI
-	char bufMySqlHost[64] = "citmalumnes.upc.es";
+	char bufMySqlHost[64] = "sql2.freemysqlhosting.net";
 	char bufMySqlPort[64] = "3306";
-	char bufMySqlDatabase[64] = "database";
+	char bufMySqlDatabase[64] = "sql2262471";
 	char bufMySqlTable[64] = "messages";
-	char bufMySqlUsername[64] = "username";
-	char bufMySqlPassword[64] = "password";
+	char bufMySqlUsername[64] = "sql2262471";
+	char bufMySqlPassword[64] = "gY8!qZ8*";
 };
 
